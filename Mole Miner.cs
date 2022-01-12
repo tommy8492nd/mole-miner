7dde2719b499b20a66fbe326883ea3a9ed1845f1
Tommy So <tommy8492nd@gmail.com>
2022-01-12 12:45:10 -0800

    commit local files to repository

---
+++ Mole Miner.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using Harmony;
+
+namespace Mole_Miner
+{
+    public class Mole_Miner
+    {
+        [HarmonyPatch(typeof(AutoMinerConfig))]
+        [HarmonyPatch(nameof(AutoMinerConfig.CreateBuildingDef))]
+
+        public static class AutoMinerConfig_CreateBuildingDef_Patch
+        {
+            public static void Postfix(ref BuildingDef __result)
+            {
+                __result.Entombable = false;
+            }
+        }
+        [HarmonyPatch(typeof(AutoMinerConfig))]
+        [HarmonyPatch(nameof(AutoMinerConfig.DoPostConfigureComplete))]
+        public class AutoMinerConfig_DoPostConfigureComplete_Patch
+        {
+            public static void Postfix(ref GameObject go)
+            {
+                go.GetComponent<KPrefabID>().AddTag(GameTags.Bunker, false);
+            }
+        }
+    }
+}
